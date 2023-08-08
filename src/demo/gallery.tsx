9565ea19b0ffe650647c4ed22cfb89d7114f5458
cgm <377342130@qq.com>
2023-08-08 15:50:56 +0800

    feat: first react component

---
+++ src/demo/gallery.tsx
@@ -0,0 +1,31 @@
+import { Profile } from './profile';
+
+type Profiles = {
+	key: number
+	name: string
+}
+
+const profiles: Profiles[] = Array(9).fill({ key: 0 }).map((_profile: Profiles, index) => ({
+	key: index + 1,
+	name: `Taylor ${index + 1}`
+}))
+ 
+export const Gallery = () => {
+	const gallery = profiles.map(profile => 
+		<div className="profile" key={profile.key}>
+			<Profile />
+			<div className="account">
+				{ profile.name }
+			</div>
+		</div>
+	)
+
+	return (
+		<>
+			<h1 style={{textAlign: 'center'}}>This is a Gallery for Taylor</h1>
+			<div className="gallery">
+				{ gallery }
+			</div>
+		</>
+	)
+}
---
+++ src/demo/profile.tsx
@@ -0,0 +1,7 @@
+export const Profile = () => (
+	<img
+		src='https://mr7ish.github.io/Taylor.jpg'
+        alt="taylor"
+        loading="lazy"
+	/>
+)
