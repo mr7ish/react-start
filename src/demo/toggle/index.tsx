ea010821e5bde7468a6cb6dc93002c4b75c811eb
cgm <377342130@qq.com>
2023-10-10 18:23:07 +0800

    feat: toggle component

---
+++ src/demo/toggle/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "../svg";
+import './index.less';
+
+
+const Toggle = () => {
+    const [isLight, setIsLight] = useState(true);
+
+    const changeStyle = (_isLight: boolean) => {
+        if (_isLight) return document.documentElement.classList.remove('dark');
+        return document.documentElement.classList.add('dark');
+    }
+
+    useEffect(() => {
+        changeStyle(isLight);
+    }, [isLight]);
+
+    const onToggle = () => {
+        setIsLight(!isLight);
+    }
+
+    return (
+        <div
+            className='toggle'
+            onClick={onToggle}
+            style={{
+                borderColor: `${isLight ? 'rgba(60, 60, 67, .29)' : 'rgba(82, 82, 89, .68)'}`,
+            }}
+            title={isLight ? 'light theme' : 'dark theme'}
+        >
+            <div
+                className="wrapper"
+                style={{
+                    transform: `translateX(${isLight ? 0 : 18}px)`,
+                    backgroundColor: `${isLight ? '#fff' : '#000'}`,
+                }}
+            >
+                {
+                    isLight ?
+                        <Sun /> :
+                        <Moon />
+                }
+            </div>
+        </div>
+    )
+
+
+}
+
+export default Toggle;
