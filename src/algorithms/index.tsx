fbd7f4884ede212ae72f9c56e24d9da7d18c8fee
cgm <377342130@qq.com>
2023-10-11 09:43:19 +0800

    feat: clean code

--- src/algorithms/index.tsx
+++ src/algorithms/index.tsx
@@ -1,10 +1,6 @@
 // import { TwoSum } from "./twoSum";
 // import { TwoNumPlus } from './twoNumPlus';
 
-import { useIdle } from "@/utils/hooks";
-import { useEffect } from "react";
-
-
 const Algorithems = () => {
     // const towsum = new TwoSum(6);
     // towsum.output('hashMap').output('exhaust');
@@ -12,25 +8,10 @@ const Algorithems = () => {
     // const twoNumPlus = new TwoNumPlus();
     // twoNumPlus.log();
     // twoNumPlus.sum();
-    const { listen, isIdle, idleTime, cleanup } = useIdle({ timeout: 5000 });
-
-    useEffect(() => {
-        console.log('isIdle =>', isIdle);
-    }, [isIdle]);
-
-    useEffect(() => {
-        console.log('idleTime =>', idleTime);
-
-    }, [idleTime]);
 
     return (
         <>
-            <button
-                onClick={listen}
-            >listen</button>
-            <button
-                onClick={cleanup}
-            >stop</button>
+
         </>
     );
 }
